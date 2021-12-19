44f8d862f73c4d9fc4905ae03cc153d86b5002af
cristiansierramintic <91471948+cristiansierramintic@users.noreply.github.com>
2021-12-19 09:27:56 -0500

    first commit

---
+++ src/main/java/com/ciclo4/reto4/repository/crud/OrderCrudRepository.java
@@ -0,0 +1,15 @@
+package com.ciclo4.reto4.repository.crud;
+
+import com.ciclo4.reto4.model.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface OrderCrudRepository extends MongoRepository<Order, Integer> {
+    List<Order> findBySalesManZone(String zone);
+    List<Order> findBySalesManId(Integer id);
+    List<Order> findBySalesManIdAndStatus(Integer id, String status);
+    List<Order> findByRegisterDayAndSalesManId(Date registerDay, Integer id);
+
+}
---
+++ src/main/java/com/ciclo4/reto4/repository/crud/ProductCrudRepository.java
@@ -0,0 +1,11 @@
+package com.ciclo4.reto4.repository.crud;
+
+import com.ciclo4.reto4.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ProductCrudRepository extends MongoRepository<Product, Integer> {
+    public List<Product> findByPrice(double price);
+    public List<Product> findByDescriptionContainingIgnoreCase(String description);
+}
